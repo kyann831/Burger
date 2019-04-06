5094adb30c9d935e614d4c361b47cce1d5c4ed31
Ky-Ann Long <kyannlong@gmail.com>
2019-04-06 15:26:24 -0400

    initial files created

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon');
+INSERT INTO burgers (burger_name) VALUES ('Veggie');
+INSERT INTO burgers (burger_name) VALUES ('Cheese');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese', false);
