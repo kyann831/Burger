ead4db857a28ba0762a33b62671d9eebe93fb15e
Ky-Ann Long <kyannlong@gmail.com>
2019-04-08 15:07:56 -0400

    heroku databases

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,12 +3,12 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (burger.js) to use its database functions.
-var cat = require("../models/burger.js");
+var burger = require("../models/burger.js");
 
 router.get("/", function(req, res) {
-    cat.all(function(data) {
+    burger.all(function(data) {
       var hbsObject = {
-        cats: data
+        burgers: data
       };
       console.log(hbsObject);
       res.render("index", hbsObject);
@@ -16,10 +16,10 @@ router.get("/", function(req, res) {
   });
   
   router.post("/api/burgers", function(req, res) {
-    cat.create([
-      "name", "sleepy"
+    burger.create([
+      "name", "devoured"
     ], [
-      req.body.name, req.body.sleepy
+      req.body.burger_name, req.body.devoured
     ], function(result) {
       // Send back the ID of the new quote
       res.json({ id: result.insertId });
@@ -31,7 +31,7 @@ router.get("/", function(req, res) {
   
     console.log("condition", condition);
   
-    cat.update({
+    burger.update({
       devoured: req.body.devoured
     }, condition, function(result) {
       if (result.changedRows == 0) {
@@ -46,7 +46,7 @@ router.get("/", function(req, res) {
   router.delete("/api/burgers/:id", function(req, res) {
     var condition = "id = " + req.params.id;
   
-    cat.delete(condition, function(result) {
+    burger.delete(condition, function(result) {
       if (result.affectedRows == 0) {
         // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO burgers (burger_name) VALUES ('Bacon');
-INSERT INTO burgers (burger_name) VALUES ('Veggie');
-INSERT INTO burgers (burger_name) VALUES ('Cheese');
 INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Avocado, false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bean', false);
+
